17b18e4dccca8a4f048091d2f93ce38a60e5ecc4
Guilherme Tiosso <guilherme.tiosso@gmail.com>
2019-04-07 00:54:49 -0300

    Program to Delete Data on DB, Integrity Exception Created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,4 @@
-// Atualizando dados no banco de dados
+// Removendo dados do banco de dados
 // PreparedStatement = Interface responsável por montar a consulta sql e passar o parametros depois
 
 package application;
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import model.exceptions.DbIntegrityException;
 import model.services.DB;
 
 public class Program {
@@ -22,26 +23,25 @@ public class Program {
 			
 			// Instanciando o PreparedStatement com a operação
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)"
+					+ "Id = ?"
 					);
 			
 			// Substituindo os valores "?" por dados
 			// O comando set(String, Int, Double...) serve para alterarmos os valores "?" pelo dado pertinente a cada tipo
 			// o Campo "1" é o indice de cada valor "?", ou seja, 1 é o primeiro ?, 2 o segundo, 3 o terceiro.... e assim em diante;
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 2);
 			
-			// Realizando de fato a inserção dos dados, retorna o numero de linhas afetadas
+			// Realizando de fato a execução, retorna o numero de linhas afetadas
 			int rowsAffect = st.executeUpdate();
 			
 			System.out.println("Done! - Rows Affected: " + rowsAffect);
 			
 		} 
 		catch (SQLException e) {
-			e.printStackTrace();
+			// Lançando a Exceção Personalizada
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
